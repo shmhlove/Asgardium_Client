1f69c859c4e5e05a9c4acd0a8282caba2e65797c
SangHo Lee <shmhlove@naver.com>
2019-02-14 23:05:23 +0900

    [로비] 메인메뉴 UI 수정 ( 선택 기능 )

---
+++ Assets/02_script/ui/utility/SHUIButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class SHUIButton : UIButton
+{
+	public override void SetState (State state, bool immediate)
+	{
+		base.SetState(state, immediate);
+        isEnabled = (State.Disabled != state);
+	}
+}

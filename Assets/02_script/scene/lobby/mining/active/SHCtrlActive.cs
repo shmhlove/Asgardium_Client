96d411a1dac984b9604ce0518f312f6a6042f6b5
SangHo Lee <shmhlove@naver.com>
2019-02-19 23:40:25 +0900

    [마이닝] 클래스 구조잡는 중

---
+++ Assets/02_script/scene/lobby/mining/active/SHCtrlActive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class SHCtrlActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

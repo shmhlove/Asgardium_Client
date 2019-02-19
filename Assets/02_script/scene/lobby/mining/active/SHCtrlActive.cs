d049471b054fea6bb4de83e44a480365e4ab0a7b
SangHo Lee <shmhlove@naver.com>
2019-02-19 23:45:15 +0900

    [마이닝] 작업 리스트 정리

--- Assets/02_script/scene/lobby/mining/active/SHCtrlActive.cs
+++ Assets/02_script/scene/lobby/mining/active/SHCtrlActive.cs
@@ -5,15 +5,7 @@ using System.Collections.Generic;
 
 public class SHCtrlActive : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 메인 액티브 정보 처리 : 번개 충전 및 시간 처리
+    // 필터동작시 이벤트 받아 스크롤뷰 재구성, 필터내용은 저장하여 켜질때 그대로 셋팅될 수 있도록
+    // 스크롤뷰 구성 - 출력해야할 리스트 서버로 부터 받아 슬롯구성할 수 있도록 스크롤뷰클래스에 전달
 }

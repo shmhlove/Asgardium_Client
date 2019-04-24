36ca130eb518eeb53822de591fd6893f420a0c7c
SangHo Lee <shmhlove@naver.com>
2019-04-25 05:02:53 +0900

    [기타] 오타수정

--- Assets/02_script/logic/login/business/SHBusinessLogin.cs
+++ Assets/02_script/logic/login/business/SHBusinessLogin.cs
@@ -52,7 +52,7 @@ public class SHBusinessLogin : MonoBehaviour
     {
         if (false == SHUtils.IsValidEmail(strEmail))
         {
-            Single.BusinessGlobal.ShowAlertUI(new SHReply(new SHError(eErrorCode.Auth_InvalidEmaile, "")));
+            Single.BusinessGlobal.ShowAlertUI(new SHReply(new SHError(eErrorCode.Auth_InvalidEmail, "")));
             return;
         }
 

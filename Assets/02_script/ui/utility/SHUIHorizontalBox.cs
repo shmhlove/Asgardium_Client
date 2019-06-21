d171dd8474c37b00e1bc17bb9f648a65378fe210
SangHo Lee <shmhlove@naver.com>
2019-06-21 23:44:42 +0900

    [마이닝] 필터 기능 개발 중

---
+++ Assets/02_script/ui/utility/SHUIHorizontalBox.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public enum eHorizontalAlignment
+{
+    Left,
+    Center,
+    Right
+}
+
+public class SHUIHorizontalBox : MonoBehaviour
+{
+    public float m_fSlotSize;
+    public float m_fGapSize;
+    public int m_iTotalCount;
+    public eHorizontalAlignment m_eAlignment;
+
+    public List<GameObject> m_pSlots = new List<GameObject>();
+
+    public void SetSlot(GameObject pSlot)
+    {
+        m_pSlots.Add(pSlot);
+        ResetPosition();
+    }
+
+    [FuncButton]
+    public void ResetPosition()
+    {
+        int iSlotLen = m_pSlots.Count;
+        float fTotalSize = 0;
+        float fStartPosition = 0;
+        switch(m_eAlignment)
+        {
+            case eHorizontalAlignment.Left:
+                fTotalSize = ((m_iTotalCount-1) * (m_fSlotSize + m_fGapSize)) - m_fGapSize;
+                fStartPosition = -(fTotalSize/2);
+            break;
+            case eHorizontalAlignment.Center:
+                fTotalSize = ((iSlotLen-1) * (m_fSlotSize + m_fGapSize)) - m_fGapSize;
+                fStartPosition = -(fTotalSize/2);
+            break;
+            case eHorizontalAlignment.Right:
+                fTotalSize = ((iSlotLen-1) * (m_fSlotSize + m_fGapSize)) - m_fGapSize;
+                fStartPosition = -(fTotalSize/2);
+            break;
+        }
+        
+        for (int iLoop = 0; iLoop < iSlotLen; ++iLoop)
+        {
+            var position = m_pSlots[iLoop].transform.localPosition;
+            position.x = fStartPosition + ((iLoop * (m_fSlotSize + m_fGapSize)) - (m_fGapSize/2));
+            m_pSlots[iLoop].transform.localPosition = position;
+        }
+    }
+}

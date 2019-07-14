14b80f70da68cecd009d1a2325586df34d6e2286
shmhlove <shmhlove@naver.com>
2019-07-15 01:11:53 +0900

    [저장소] 스크롤뷰 동적 아이템 크기 처리 중(UITable사용)

---
+++ Assets/02_script/logic/lobby/storage/ui/SHUITableViewTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHUITableViewTest : MonoBehaviour
+{
+    public UITable m_pTableView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [FuncButton]
+    void Reflash()
+    {
+        m_pTableView.repositionNow = true;
+    }
+}

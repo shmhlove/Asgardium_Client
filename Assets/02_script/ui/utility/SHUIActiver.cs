4ffe768579869a4b112c3ccac57c9cd167fc1adf
SangHo Lee <shmhlove@naver.com>
2019-07-15 23:14:46 +0900

    [저장소] 스크롤뷰 동적 아이템 크기, Collapsibleset 처리 중

---
+++ Assets/02_script/ui/utility/SHUIActiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHUIActiver : MonoBehaviour
+{
+    public void OnEventActive(bool bIsActive)
+    {
+        NGUITools.SetActive(gameObject, bIsActive);
+    }
+
+    public void OnEventDisable(bool bIsDisable)
+    {
+        NGUITools.SetActive(gameObject, !bIsDisable);
+    }
+}
